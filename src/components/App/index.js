b5787140f0ca42b69fd553230babfd1378ad0758
Kowak <shwkxz@gmail.com>
2022-04-25 05:03:29 +0000

    Create ContactList Header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,8 @@ import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import Header from '../Header';
+import ContactsList from '../ContactsList';
+
 import { Container } from './styles';
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
       <GlobalStyles />
       <Container>
         <Header />
+        <ContactsList />
       </Container>
     </ThemeProvider>
   );
