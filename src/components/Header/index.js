1752b90ee317457fe5b6d165febde3530957d5e0
Kowak <shwkxz@gmail.com>
2022-04-27 03:15:36 +0000

    Homepage and header refactor

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import { Container, InputSearchContainer } from './styles';
+import { Container } from './styles';
 
 import logo from '../../assets/images/Logo.svg';
 
@@ -6,9 +6,7 @@ export default function Header() {
   return (
     <Container>
       <img src={logo} alt="MyContacts" width="201" />
-      <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar Contato" />
-      </InputSearchContainer>
+
     </Container>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,92 @@
+import {
+  Container, InputSearchContainer, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/Arrow.svg';
+import edit from '../../assets/images/icons/Edit.svg';
+import trash from '../../assets/images/icons/Trash.svg';
+
 export default function Home() {
-  return 'Home';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar Contato" />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 Contatos</strong>
+        <a href="/">Novo Contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Nome-Contato</strong>
+              <small>categoria</small>
+            </div>
+            <span>email@email.com</span>
+            <span>(99) 4002-8922</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Nome-Contato</strong>
+              <small>categoria</small>
+            </div>
+            <span>email@email.com</span>
+            <span>(99) 4002-8922</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Nome-Contato</strong>
+              <small>categoria</small>
+            </div>
+            <span>email@email.com</span>
+            <span>(99) 4002-8922</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }
